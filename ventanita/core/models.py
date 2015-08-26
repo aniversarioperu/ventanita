e394020a13988911a7c57ac51b79c1eb9d47bfe8
Droper <pmunoz@gmail.com>
2015-08-25 19:30:40 -0500

    Modifications to the model, just delete the database and the migrations and start again.

--- ventanita/core/models.py
+++ ventanita/core/models.py
@@ -1,3 +1,4 @@
+# Copyright 2015 by Pedro Muñoz del Rio (pmunozdelrio). All rights reserved.
 # Copyright 2015 by Pedro Palacios (Wesitos). All rights reserved.
 # Revisions 2015 copyright by AniversarioPeru. All rights reserved.
 # This code is part of the Ventanita distribution and governed by its
@@ -7,79 +8,22 @@
 from django.db import models
 
 
-class Candidato(models.Model):
-    candidato_jne_id = models.IntegerField(help_text='ID asignado por el JNE')
+GENDER = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
 
-    # Datos Personales
-    dni = models.CharField(max_length=8)
-    nombres = models.CharField(max_length=300)
-    apellido_materno = models.CharField(max_length=300)
-    apellido_paterno = models.CharField(max_length=300)
-    sexo = models.CharField(max_length=300)
-    email = models.CharField(max_length=300)
-    org_politica = models.CharField(max_length=300)
-
-    # Familia
-    madre = models.CharField(max_length=300)
-    conyuge = models.CharField(max_length=300)
-    padre = models.CharField(max_length=300)
-
-    # Postulacion
-    postulacion_cargo = models.CharField(max_length=300)
-    postulacion_ubigeo = models.CharField(max_length=300)
-    postulacion_distrito = models.CharField(max_length=300)
-    postulacion_provincia = models.CharField(max_length=300)
-    postulacion_departamento = models.CharField(max_length=300)
-    postulacion_designacion = models.TextField()
-
-    # Nacimiento
-    nacimiento_pais = models.CharField(max_length=300)
-    nacimiento_ubigeo = models.CharField(max_length=300)
-    nacimiento_fecha = models.DateField(null=True, blank=True)
-    nacimiento_distrito = models.CharField(max_length=300)
-    nacimiento_provincia = models.CharField(max_length=300)
-    nacimiento_departamento = models.CharField(max_length=300)
-
-    # Residencia
-    residencia_lugar = models.TextField(blank=True)
-    residencia_ubigeo = models.CharField(max_length=300, blank=True)
-    residencia_distrito = models.CharField(max_length=300, blank=True)
-    residencia_tiempo = models.CharField(max_length=300, blank=True)
-    residencia_provincia = models.CharField(max_length=300, blank=True)
-    residencia_departamento = models.CharField(max_length=300, blank=True)
-
-    # bienes_muebles = relationship("BienMueble", backref="candidato")
-    # bienes_inmuebles = relationship("BienInmueble", backref="candidato")
-    # otra_experiencia = relationship("OtraExperiencia", backref="candidato")
-    # militancia = relationship("Militancia", backref="candidato")
-    # civil = relationship("Civil", backref="candidato")
-
-    # educacion_basica_primaria = relationship("Primaria", backref="candidato")
-    # educacion_basica_secundaria = relationship("Secundaria", backref="candidato")
-    # educacion_superior_postgrado = relationship("Postgrado", backref="candidato")
-    # educacion_superior_universitario = relationship("Universitario", backref="candidato")
-    # educacion_superior_tecnico = relationship("Tecnico", backref="candidato")
-    # partidario = relationship("Partidario", backref="candidato")
-    # eleccion = relationship("Eleccion", backref="candidato")
-    # experiencia = relationship("Experiencia", backref="candidato")
-    # observaciones = relationship("Observacion", backref="candidato")
+""
+class TipoMedioContacto(models.Model):
+    nombre = models.CharField(max_length=50)
 
 
-class BienMueble(models.Model):
-    bien = models.CharField(max_length=300)
-    tipo = models.CharField(max_length=300)
-    descripcion = models.CharField(max_length=300)
-    caracteristicas = models.CharField(max_length=300)
-    valor = models.IntegerField()
-    candidato = models.ForeignKey('Candidato')
+class TipoBienMueble(models.Model):
+    nombre = models.CharField(max_length=50)
 
 
-class BienInmueble(models.Model):
-    registro = models.CharField(max_length=300)
-    valor = models.IntegerField()
-    tipo = models.CharField(max_length=300)
-    direccion = models.CharField(max_length=300)
-    candidato = models.ForeignKey('Candidato')
+class TipoBienInmueble(models.Model):
+    nombre = models.CharField(max_length=50)
 
 
 class OtraExperiencia(models.Model):
@@ -113,7 +57,7 @@ class Penal(models.Model):
     juzgado = models.CharField(max_length=300)
     fallo = models.CharField(max_length=300)
     fecha_sentencia = models.DateField()
-    candidato = models.ForeignKey('Candidato')
+    candidato = models.ForeignKey('Candidato', null=True)
 
 
 class Partidario(models.Model):
@@ -122,7 +66,7 @@ class Partidario(models.Model):
     org_politica = models.CharField(max_length=300)
     inicio = models.IntegerField()
     fin = models.IntegerField()
-    candidato = models.ForeignKey('Candidato')
+    candidato = models.ForeignKey('Candidato', null=True)
 
 
 class Eleccion(models.Model):
@@ -135,60 +79,26 @@ class Eleccion(models.Model):
     distrito = models.CharField(max_length=300)
     ambito = models.CharField(max_length=300)
     org_politica = models.CharField(max_length=300)
-    candidato = models.ForeignKey('Candidato')
+    candidato = models.ForeignKey('Candidato', null=True)
 
 
-class Experiencia(models.Model):
-    sector = models.CharField(max_length=300)
-    cargo = models.IntegerField()
-    provincia = models.CharField(max_length=300)
-    empleador = models.CharField(max_length=300)
-    distrito = models.CharField(max_length=300)
-    inicio = models.IntegerField()
-    fin = models.IntegerField()
-    departamento = models.CharField(max_length=300)
-    candidato = models.ForeignKey('Candidato')
+class Sector(models.Model):
+    nombre = models.CharField(max_length=50)
 
+class CentroTrabajo(models.Model):
+    nombre = models.CharField(max_length=100)
+    sector = models.ForeignKey(Sector)
 
-class Estudio(models.Model):
-    PRIMARIA = 'primaria'
-    SECUNDARIA = 'secundaria'
-    TECNICA = 'tecnica'
-    UNIVERSITARIA = 'universitaria'
-    POSTGRADO = 'postgrado'
-    TYPE_STUDY_CHOICES = (
-        (PRIMARIA, 'primaria'),
-        (TECNICA, 'tecnica'),
-        (SECUNDARIA, 'secundaria'),
-        (UNIVERSITARIA, 'universitaria'),
-        (POSTGRADO, 'postgrado'),
-    )
+class TipoGradoAcademico(models.Model):
+    nombre = models.CharField(max_length=50)
 
-    candidato = models.ForeignKey('Candidato')
-    institucion_educativa = models.ForeignKey('InstitucionEducativa')
-    tipo_de_estudio = models.CharField(max_length=300, choices=TYPE_STUDY_CHOICES, blank=True,
-                                       help_text='Colegio, instituto, universidad y grado de instrucción.')
-    concluido = models.NullBooleanField()
-    inicio = models.IntegerField()
-    fin = models.IntegerField()
-
-    codigo_anr = models.TextField(blank=True)
-    # tecnica
-    curso = models.CharField(max_length=300, help_text='Nombre de estudio')
-    especialidad = models.CharField(max_length=300)
-
-    # universitaria
-    grado_titulo = models.CharField(max_length=300)
-    facultad = models.CharField(max_length=300)
-    carrera = models.CharField(max_length=300)
-
-    # postgrado
-    tipo_de_grado = models.TextField(blank=True)
-    tipo_postgrado = models.TextField(blank=True)
-    otro_tipo_documento = models.TextField(blank=True)
+class TipoInstitucionEducativa(models.Model):
+    nombre = models.CharField(max_length=40)
+    grado_academico = models.ManyToManyField(TipoGradoAcademico)
 
 
 class InstitucionEducativa(models.Model):
+    tipo_institucion_educativa = models.ForeignKey(TipoInstitucionEducativa, null=True)
     sha1 = models.CharField(max_length=40, db_index=True,
                             help_text='This is only used when importing data.')
     nombre = models.CharField(max_length=300)
@@ -199,25 +109,130 @@ class InstitucionEducativa(models.Model):
     distrito = models.CharField(max_length=300)
 
 
+class Candidato(models.Model):
+    candidato_jne_id = models.IntegerField(help_text='ID asignado por el JNE')
+
+    # Datos Personales
+    dni = models.CharField(max_length=8, primary_key=True)
+    nombres = models.CharField(max_length=300)
+    apellido_materno = models.CharField(max_length=300)
+    apellido_paterno = models.CharField(max_length=300)
+    sexo = models.CharField(max_length=2,
+                            choices=GENDER,
+                            default='M')
+
+
+    org_politica = models.CharField(max_length=300)
+
+    medio_contacto = models.ManyToManyField(TipoMedioContacto, through='MedioContacto')
+
+    # Familia
+    madre = models.CharField(max_length=300)
+    conyuge = models.CharField(max_length=300)
+    padre = models.CharField(max_length=300)
+
+    # Postulacion
+    postulacion_cargo = models.CharField(max_length=300)
+    postulacion_ubigeo = models.CharField(max_length=300)
+    postulacion_distrito = models.CharField(max_length=300)
+    postulacion_provincia = models.CharField(max_length=300)
+    postulacion_departamento = models.CharField(max_length=300)
+    postulacion_designacion = models.TextField()
+
+    # Nacimiento
+    nacimiento_pais = models.CharField(max_length=300)
+    nacimiento_ubigeo = models.CharField(max_length=300)
+    nacimiento_fecha = models.DateField(null=True, blank=True)
+    nacimiento_distrito = models.CharField(max_length=300)
+    nacimiento_provincia = models.CharField(max_length=300)
+    nacimiento_departamento = models.CharField(max_length=300)
+
+    # Residencia
+    residencia_lugar = models.TextField(blank=True)
+    residencia_ubigeo = models.CharField(max_length=300, blank=True)
+    residencia_distrito = models.CharField(max_length=300, blank=True)
+    residencia_tiempo = models.CharField(max_length=300, blank=True)
+    residencia_provincia = models.CharField(max_length=300, blank=True)
+    residencia_departamento = models.CharField(max_length=300, blank=True)
+
+    estudios = models.ManyToManyField(InstitucionEducativa, through='Estudio')
+    #trabajos = models.ManyToManyField(CentroTrabajo, through='ExperienciaLaboral')
+
+    # bienes_muebles = relationship("BienMueble", backref="candidato")
+    # bienes_inmuebles = relationship("BienInmueble", backref="candidato")
+    # otra_experiencia = relationship("OtraExperiencia", backref="candidato")
+    # militancia = relationship("Militancia", backref="candidato")
+    # civil = relationship("Civil", backref="candidato")
+
+    # partidario = relationship("Partidario", backref="candidato")
+    # eleccion = relationship("Eleccion", backref="candidato")
+    # experiencia = relationship("Experiencia", backref="candidato")
+    # observaciones = relationship("Observacion", backref="candidato")
+
+
+class BienMueble(models.Model):
+    nombre = models.CharField(max_length=300)
+    tipo_bien = models.ForeignKey(TipoBienMueble, null=True)
+    descripcion = models.CharField(max_length=300)
+    caracteristicas = models.CharField(max_length=300)
+    valor = models.IntegerField()
+    candidato = models.ForeignKey(Candidato, null=True)
+
+
+class BienInmueble(models.Model):
+    registro = models.CharField(max_length=300)
+    valor = models.IntegerField()
+    tipo_bien = models.ForeignKey(TipoBienInmueble, null=True)
+    direccion = models.CharField(max_length=300)
+    candidato = models.ForeignKey(Candidato, null=True)
+
+
+class Estudio(models.Model):
+    institucion_educativa = models.ForeignKey(InstitucionEducativa)
+    tipo_de_estudio = models.ForeignKey(TipoGradoAcademico, null=True)
+    candidato = models.ForeignKey(Candidato, null=True)
+    concluido = models.NullBooleanField()
+    inicio = models.DateField()
+    fin = models.DateField()
+    codigo_anr = models.TextField(blank=True)
+
+
+class MedioContacto(models.Model):
+    tipo_medio = models.ForeignKey(TipoMedioContacto, null=True)
+    candidato = models.ForeignKey(Candidato, null=True)
+    contacto = models.CharField(max_length=50)
+
+
+class ExperienciaLaboral(models.Model):
+    cargo = models.CharField(max_length=100)
+    departamento = models.CharField(max_length=300)
+    provincia = models.CharField(max_length=300)
+    distrito = models.CharField(max_length=300)
+    empleador = models.ForeignKey(CentroTrabajo, null=True)
+    candidato = models.ForeignKey(Candidato, null=True)
+    inicio = models.DateField()
+    fin = models.DateField()
+
+
 class Observacion(models.Model):
     referencia = models.CharField(max_length=300)
     anotacion = models.CharField(max_length=2000)
-    candidato = models.ForeignKey('Candidato')
+    candidato = models.ForeignKey('Candidato', null=True)
 
 
 # Registro de morosos por alimentos. REDAM.
 class DeudorRedam(models.Model):
     """Demandado"""
-    dni = models.CharField(max_length=8)
-    apellido_paterno = models.CharField(max_length=100)
-    apellido_materno = models.CharField(max_length=100)
-    nombres = models.CharField(max_length=200)
+    dni = models.TextField()
+    paternal_surname = models.TextField()
+    maternal_surname = models.TextField()
+    given_names = models.TextField()
     url = models.URLField()
-    debe = models.FloatField()
+    debt = models.FloatField()
 
 
-class DeudorRedamVinculo(models.Model):
+class DeudorRedamBond(models.Model):
     """Demandante"""
-    deudor = models.ForeignKey('DeudorRedam')
-    vinculo = models.CharField(max_length=100)
-    nombre_completo = models.CharField(max_length=300)
+    debtor = models.ForeignKey('DeudorRedam', null=True)
+    bond_type = models.TextField()
+    full_name = models.TextField()
